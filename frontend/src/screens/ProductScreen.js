1ea774ba203ed48aa95256d295eb6146636a5ac2
Sudhanshu Kumar <sudh2810@gmail.com>
2020-12-16 19:37:40 +0530

    temp frontend and backend created

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-    const product = products.find( p => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            // console.log(data)
+            setProduct(data)
+        }
+        fetchProduct()
+    })
+
     return (
         <>
+            <Row style={{justifyContent:'flex-end'}} >
             <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
+            </Row>
             <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid />
@@ -46,7 +58,7 @@ const ProductScreen = ({ match }) => {
                                 <Row>
                                     <Col>Status:</Col>
                                     <Col>
-                                        {product.countInStock ? 'In Stoct' : 'Out of Stock'}
+                                        {product.countInStock ? 'In Stock' : 'Out of Stock'}
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
