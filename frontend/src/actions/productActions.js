6e98cb63423908e7ebd6482369a0c720c9087cbb
Sudhanshu Kumar <sudh2810@gmail.com>
2020-12-29 11:04:46 +0530

    product create, update, and delete backend

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -8,7 +8,10 @@ import {
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DELETE_FAIL,
     PRODUCT_DELETE_SUCCESS,
-    PRODUCT_DELETE_REQUEST
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL
 } from '../constants/productConstants'
 
 // redux thunk allows to add a function within a function
@@ -79,6 +82,38 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DELETE_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message, 
+        })
+    }
+}
+
+export const createProduct = (id) => async (dispatch, getState) => {
+    // the inner function takes getState as the 2nd parameter
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST,
+        })
+
+        const { userLogin: { userInfo }} = getState()
+        // destructing to get the token
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.post(`/api/products`, {}, config)
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_FAIL,
             payload: error.response && error.response.data.message
                 ? error.response.data.message
                 : error.message, 
